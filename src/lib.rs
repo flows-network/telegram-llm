3f695156b91ad6d7ae80c8bfc9011e47c8e44298
Michael Yuan <michael@secondstate.io>
2023-03-20 08:01:57 +0000

    Remove the hacks
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use tg_flows::{listen_to_update, Telegram, UpdateKind};
 use openai_flows::{chat_completion, ChatOptions, ChatModel};
 use std::env;
-use flowsnet_platform_sdk::write_error_log;
 
 #[no_mangle]
 pub fn run() {
@@ -18,6 +17,23 @@ pub fn run() {
             let text = msg.text().unwrap_or("");
             let chat_id = msg.chat.id;
 
+            let prompt = "You are a helpful assistant answering questions on Telegram.\n\n If someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\" \n\n".to_owned() + &text + "\n```";
+            let co = ChatOptions {
+                model: ChatModel::GPT4,
+                restart: text.eq_ignore_ascii_case("restart"),
+                restarted_sentence: Some(&prompt)
+            };
+
+            let c = chat_completion(&openai_key_name, &chat_id.to_string(), &text, &co);
+            if let Some(c) = c {
+                if c.restarted {
+                    _ = tele.send_message(chat_id, "I am starting a new conversation since it has been over 10 minutes from your last reply. You can also tell me to restart by typing \"restart\" into the chat.\n\n".to_string() + &c.choice);
+                } else {
+                    _ = tele.send_message(chat_id, c.choice);
+                }
+            }
+
+            /*
             let res = tele.send_message(chat_id, "thinking ...");
 
             match res {
@@ -44,7 +60,7 @@ pub fn run() {
                     write_error_log!(e.to_string());
                 }
             }
-
+            */
         }
     });
 }
