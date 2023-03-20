3c62a5aa06833d258f13568128c7106fd32aeffd
Michael Yuan <michael@secondstate.io>
2023-03-20 01:18:17 +0000

    Try to fix issues
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use tg_flows::{listen_to_update, Telegram, UpdateKind};
 use openai_flows::{chat_completion, ChatOptions, ChatModel};
 use std::env;
+use flowsnet_platform_sdk::write_error_log;
 
 #[no_mangle]
 pub fn run() {
@@ -15,21 +16,33 @@ pub fn run() {
     listen_to_update(telegram_token, |update| {
         if let UpdateKind::Message(msg) = update.kind {
             let text = msg.text().unwrap_or("");
+            let chat_id = msg.chat.id;
 
-            let prompt = "You are a helpful assistant answering questions on Telegram. In your response, you can use simple markdown text to format your answers.\n\n".to_owned() + &text + "\n```";
-            let co = ChatOptions {
-                model: ChatModel::GPT4,
-                restart: text.eq_ignore_ascii_case("restart"),
-                restarted_sentence: Some(&prompt)
-            };
+            let res = tele.send_message(chat_id, "Lemme think ...");
 
-            let c = chat_completion(&openai_key_name, &msg.chat.id.to_string(), &text, &co);
-            if let Some(c) = c {
-                if c.restarted {
-                    _ = tele.send_message(msg.chat.id, "Let's start a new conversation!".to_string());
+            match res {
+                Ok(m) => {
+                    let prompt = "You are a helpful assistant answering questions on Telegram. In your response, you can use simple markdown text to format your answers.\n\n".to_owned() + &text + "\n```";
+                    let co = ChatOptions {
+                        model: ChatModel::GPT4,
+                        restart: text.eq_ignore_ascii_case("restart"),
+                        restarted_sentence: Some(&prompt)
+                    };
+
+                    let c = chat_completion(&openai_key_name, &chat_id.to_string(), &text, &co);
+                    if let Some(c) = c {
+                        if c.restarted {
+                            _ = tele.send_message(chat_id, "Let's start a new conversation!".to_string());
+                        }
+                        _ = tele.edit_message_text(chat_id, m.id, c.choice);
+                    }
                 }
-                _ = tele.send_message(msg.chat.id, c.choice);
+
+                Err(e) => {
+                    write_error_log!(e.to_string());
+                }
             }
+
         }
     });
 }
