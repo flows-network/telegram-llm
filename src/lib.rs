af30d67f2284b961c412d48f0c16190b75af392a
Michael Yuan <michael@secondstate.io>
2023-03-29 17:43:51 +0000

    Use the new prompt
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub fn run() {
             let text = msg.text().unwrap_or("");
             let chat_id = msg.chat.id;
 
-            let prompt = "You are a helpful assistant answering questions on Telegram.\n\n If someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\" \n\n".to_owned() + &text + "\n```";
+            let prompt = "You are a helpful assistant answering questions on Telegram.\n\nIf someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\"";
             let co = ChatOptions {
                 model: ChatModel::GPT4,
                 restart: text.eq_ignore_ascii_case("restart"),
@@ -32,35 +32,6 @@ pub fn run() {
                     _ = tele.send_message(chat_id, c.choice);
                 }
             }
-
-            /*
-            let res = tele.send_message(chat_id, "thinking ...");
-
-            match res {
-                Ok(m) => {
-                    let prompt = "You are a helpful assistant answering questions on Telegram.\n\n If someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\" \n\n".to_owned() + &text + "\n```";
-                    let co = ChatOptions {
-                        model: ChatModel::GPT4,
-                        restart: text.eq_ignore_ascii_case("restart"),
-                        restarted_sentence: Some(&prompt)
-                    };
-
-                    let c = chat_completion(&openai_key_name, &chat_id.to_string(), &text, &co);
-                    if let Some(c) = c {
-                        if c.restarted {
-                            // _ = tele.send_message(chat_id, "Let's start a new conversation!".to_string());
-                            _ = tele.edit_message_text(chat_id, m.id, "I am starting a new conversation since it has been over 10 minutes from your last reply. You can also tell me to restart by typing \"restart\" into the chat.\n\n".to_string() + &c.choice);
-                        } else {
-                            _ = tele.edit_message_text(chat_id, m.id, c.choice);
-                        }
-                    }
-                }
-
-                Err(e) => {
-                    write_error_log!(e.to_string());
-                }
-            }
-            */
         }
     });
 }
