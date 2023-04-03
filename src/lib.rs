8be33e16511dfa833e4e03c1c704ffeb42c6cd42
Michael Yuan <michael@secondstate.io>
2023-04-03 06:21:53 +0000

    Update to the new openai flows SDK
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -14,20 +14,20 @@ pub fn run() {
 
     listen_to_update(telegram_token, |update| {
         if let UpdateKind::Message(msg) = update.kind {
-            let text = msg.text().unwrap_or("");
+            let mut text = msg.text().unwrap_or("");
             let chat_id = msg.chat.id;
 
-            let prompt = "You are a helpful assistant answering questions on Telegram.\n\nIf someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\"";
+            let system = "You are a helpful assistant answering questions on Telegram.\n\nIf someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\"";
             let co = ChatOptions {
                 model: ChatModel::GPT35Turbo,
                 restart: text.eq_ignore_ascii_case("restart"),
-                restarted_sentence: Some(prompt)
+                system_prompt: Some(system)
             };
-
+            if text.eq_ignore_ascii_case("restart") { text = "Hello"; }
             let c = chat_completion(&openai_key_name, &chat_id.to_string(), &text, &co);
             if let Some(c) = c {
                 if c.restarted {
-                    _ = tele.send_message(chat_id, "I am starting a new conversation since it has been over 10 minutes from your last reply. You can also tell me to restart by typing \"restart\" into the chat.\n\n".to_string() + &c.choice);
+                    _ = tele.send_message(chat_id, "I am starting a new conversation. You can always type \"restart\" to terminate the current conversation.\n\n".to_string() + &c.choice);
                 } else {
                     _ = tele.send_message(chat_id, c.choice);
                 }
