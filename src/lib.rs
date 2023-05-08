d0e4f5bd999939cf9d1a3b44af47decbb7c432a4
jinser <cmdr.jv@gmail.com>
2023-05-08 17:53:14 +0800

    feat: send placeholder text before recived chiose

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,23 @@
-use tg_flows::{listen_to_update, Telegram, UpdateKind};
-use openai_flows::{chat_completion, ChatOptions, ChatModel};
+use openai_flows::{chat_completion, ChatModel, ChatOptions};
 use std::env;
+use tg_flows::{listen_to_update, Telegram, UpdateKind};
 
 #[no_mangle]
 pub fn run() {
-    let openai_key_name: String = match env::var("openai_key_name") {
-        Err(_) => "chatmichael".to_string(),
-        Ok(name) => name,
-    };
+    let openai_key_name = env::var("openai_key_name").unwrap_or("chatmichael".to_string());
+    let placeholder_text = env::var("placeholder").unwrap_or("I'm thinking".to_string());
+    let telegram_token = env::var("telegram_token").unwrap();
 
-    let telegram_token = std::env::var("telegram_token").unwrap();
     let tele = Telegram::new(telegram_token.clone());
 
     listen_to_update(telegram_token, |update| {
         if let UpdateKind::Message(msg) = update.kind {
-            let mut text = msg.text().unwrap_or("");
+            let mut text = msg.text().unwrap_or_default();
             let chat_id = msg.chat.id;
+
+            let placeholder = tele
+                .send_message(chat_id, placeholder_text)
+                .expect("Error occurs when sending Message to Telegram");
 
             let system = "You are a helpful assistant answering questions on Telegram.\n\nIf someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\"";
             let co = ChatOptions {
@@ -25,13 +27,19 @@ pub fn run() {
                 system_prompt: Some(system),
                 retry_times: 3,
             };
-            if text.eq_ignore_ascii_case("restart") { text = "Hello"; }
-            let c = chat_completion(&openai_key_name, &chat_id.to_string(), &text, &co);
+            if text.eq_ignore_ascii_case("restart") {
+                text = "Hello";
+            }
+            let c = chat_completion(&openai_key_name, &chat_id.to_string(), text, &co);
             if let Some(c) = c {
                 if c.restarted {
-                    _ = tele.send_message(chat_id, "I am starting a new conversation. You can always type \"restart\" to terminate the current conversation.\n\n".to_string() + &c.choice);
+                    _ =   tele.edit_message_text(
+                        chat_id,
+                        placeholder.id,
+                        format!("I am starting a new conversation. You can always type \"restart\" to terminate the current conversation.\n\n{}", c.choice)
+                    );
                 } else {
-                    _ = tele.send_message(chat_id, c.choice);
+                    _ = tele.edit_message_text(chat_id, placeholder.id, c.choice);
                 }
             }
         }
