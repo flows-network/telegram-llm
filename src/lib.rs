deea2d73d96c542f2adb25ea0098f583e654b7b9
Michael Yuan <michael@secondstate.io>
2023-06-01 17:25:48 +0000

    Improve telegram bot
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -11,45 +11,62 @@ pub async fn run() -> anyhow::Result<()> {
     logger::init();
     let telegram_token = std::env::var("telegram_token").unwrap();
     let placeholder_text = std::env::var("placeholder").unwrap_or("Typing ...".to_string());
+    let system_prompt = std::env::var("system_prompt").unwrap_or("You are a helpful assistant answering questions on Telegram.".to_string());
+    let start_prompt = std::env::var("start_prompt").unwrap_or("hello".to_string());
+    let help_mesg = std::env::var("help_mesg").unwrap_or("I am your assistant on Telegram. Ask me any question! To start a new conversation, type the /restart command.".to_string());
 
     listen_to_update(&telegram_token, |update| {
         let tele = Telegram::new(telegram_token.to_string());
-        handler(tele, &placeholder_text, update)
+        handler(tele, &placeholder_text, &system_prompt, &start_prompt, &help_mesg, update)
     }).await;
 
     Ok(())
 }
 
-async fn handler(tele: Telegram, placeholder_text: &str, update: Update) {
+async fn handler(tele: Telegram, placeholder_text: &str, system_prompt: &str, start_prompt: &str, help_mesg: &str, update: Update) {
     if let UpdateKind::Message(msg) = update.kind {
         let chat_id = msg.chat.id;
         log::info!("Received message from {}", chat_id);
-        let placeholder = tele
-            .send_message(chat_id, placeholder_text)
-            .expect("Error occurs when sending Message to Telegram");
 
-        let mut text = msg.text().unwrap_or("");
-        let system = "You are a helpful assistant answering questions on Telegram.\n\nIf someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\"";
         let mut openai = OpenAIFlows::new();
         openai.set_retry_times(3);
-        let co = ChatOptions {
+        let mut co = ChatOptions {
             // model: ChatModel::GPT4,
             model: ChatModel::GPT35Turbo,
-            restart: text.eq_ignore_ascii_case("restart"),
-            system_prompt: Some(system),
+            restart: false,
+            system_prompt: Some(system_prompt),
         };
-        if text.eq_ignore_ascii_case("restart") { text = "Hello"; }
-        match openai.chat_completion(&chat_id.to_string(), &text, &co).await {
-            Ok(r) => {
-                if r.restarted {
-                    log::info!("Restart converstion for {}", chat_id);
-                    _ = tele.edit_message_text(chat_id, placeholder.id, "I am starting a new conversation. You can always type \"restart\" to terminate the current conversation.\n\n".to_string() + &r.choice);
-                } else {
+
+        let text = msg.text().unwrap_or("");
+        if text.eq_ignore_ascii_case("/help") {
+            _ = tele.send_message(chat_id, help_mesg);
+
+        } else if text.eq_ignore_ascii_case("/start") {
+            _ = tele.send_message(chat_id, help_mesg);
+            co.restart = true;
+            _ = openai.chat_completion(&chat_id.to_string(), start_prompt, &co).await;
+            log::info!("Started converstion for {}", chat_id);
+
+        } else if text.eq_ignore_ascii_case("/restart") {
+            _ = tele.send_message(chat_id, "Ok, I am starting a new conversation.");
+            co.restart = true;
+            _ = openai.chat_completion(&chat_id.to_string(), start_prompt, &co).await;
+            log::info!("Restarted converstion for {}", chat_id);
+
+        } else {
+            let placeholder = tele
+                .send_message(chat_id, placeholder_text)
+                .expect("Error occurs when sending Message to Telegram");
+
+            co.restart = false;
+            match openai.chat_completion(&chat_id.to_string(), &text, &co).await {
+                Ok(r) => {
                     _ = tele.edit_message_text(chat_id, placeholder.id, r.choice);
                 }
-            }
-            Err(e) => {
-                log::error!("OpenAI returns error: {}", e);
+                Err(e) => {
+                    _ = tele.edit_message_text(chat_id, placeholder.id, "Sorry, an error has occured. Please try again later!");
+                    log::error!("OpenAI returns error: {}", e);
+                }
             }
         }
     }
