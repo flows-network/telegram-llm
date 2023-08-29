8def5c43df5e0b342971c3446b01e8b2553d96fd
Michael Yuan <michael@secondstate.io>
2023-08-29 04:43:50 +0000

    Update to tg_flows SDK 0.3
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use serde_json::json;
-use tg_flows::{listen_to_update, Telegram, Update, UpdateKind};
+use tg_flows::{listen_to_update, Telegram, Update, UpdateKind, update_handler};
 use openai_flows::{
     chat::{ChatModel, ChatOptions},
     OpenAIFlows,
@@ -9,22 +9,21 @@ use flowsnet_platform_sdk::logger;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
-pub async fn run() -> anyhow::Result<()> {
+pub async fn on_deploy() {
+    let telegram_token = std::env::var("telegram_token").unwrap();
+    listen_to_update(telegram_token).await;
+}
+
+#[update_handler]
+async fn handler(update: Update) {
     logger::init();
     let telegram_token = std::env::var("telegram_token").unwrap();
     let placeholder_text = std::env::var("placeholder").unwrap_or("Typing ...".to_string());
     let system_prompt = std::env::var("system_prompt").unwrap_or("You are a helpful assistant answering questions on Telegram.".to_string());
     let help_mesg = std::env::var("help_mesg").unwrap_or("I am your assistant on Telegram. Ask me any question! To start a new conversation, type the /restart command.".to_string());
 
-    listen_to_update(&telegram_token, |update| {
-        let tele = Telegram::new(telegram_token.to_string());
-        handler(tele, &placeholder_text, &system_prompt, &help_mesg, update)
-    }).await;
+    let tele = Telegram::new(telegram_token.to_string());
 
-    Ok(())
-}
-
-async fn handler(tele: Telegram, placeholder_text: &str, system_prompt: &str, help_mesg: &str, update: Update) {
     if let UpdateKind::Message(msg) = update.kind {
         let chat_id = msg.chat.id;
         log::info!("Received message from {}", chat_id);
@@ -35,15 +34,15 @@ async fn handler(tele: Telegram, placeholder_text: &str, system_prompt: &str, he
             // model: ChatModel::GPT4,
             model: ChatModel::GPT35Turbo,
             restart: false,
-            system_prompt: Some(system_prompt),
+            system_prompt: Some(&system_prompt),
         };
 
         let text = msg.text().unwrap_or("");
         if text.eq_ignore_ascii_case("/help") {
-            _ = tele.send_message(chat_id, help_mesg);
+            _ = tele.send_message(chat_id, &help_mesg);
 
         } else if text.eq_ignore_ascii_case("/start") {
-            _ = tele.send_message(chat_id, help_mesg);
+            _ = tele.send_message(chat_id, &help_mesg);
             set(&chat_id.to_string(), json!(true), None);
             log::info!("Started converstion for {}", chat_id);
 
@@ -54,7 +53,7 @@ async fn handler(tele: Telegram, placeholder_text: &str, system_prompt: &str, he
 
         } else {
             let placeholder = tele
-                .send_message(chat_id, placeholder_text)
+                .send_message(chat_id, &placeholder_text)
                 .expect("Error occurs when sending Message to Telegram");
 
             let restart = match get(&chat_id.to_string()) {
